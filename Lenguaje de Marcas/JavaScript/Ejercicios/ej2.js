63b032fa9c9c31ee22787e743ee769f24e440b80
Alejandro <alejandro18032006@gmail.com>
2026-02-02 21:13:46 +0100

    Empezado a hacer Ejericio2

--- Lenguaje de Marcas/JavaScript/Ejercicios/ej2.js
+++ Lenguaje de Marcas/JavaScript/Ejercicios/ej2.js
@@ -1,3 +1,10 @@
 function aleatorioEntero(n, m) {
     return Math.floor(Math.random() * (m - n + 1)) + n;
 }
+const nAleatorio4 = aleatorioEntero(1, 4)
+const spanD4 = document.getElementById("contador")
+const boton = document.getElementById("d4")
+
+boton.addEventListener("click", () => {
+    spanD4.textContent = nAleatorio4;
+});
