7d995bb61175ff60f9681ea1ece8515e1daf4d04
Alejandro <alejandro18032006@gmail.com>
2026-02-02 19:29:26 +0100

    Documentación de geometria.py a medias

--- Entornos de Desarollo/Ejercicios/geometria.py
+++ Entornos de Desarollo/Ejercicios/geometria.py
@@ -2,26 +2,56 @@ import math
 
 
 def area_cuadrado(lado: float) -> float:
+    '''
+    :lado: un valor entero o decimal
+    :return: multiplica lado por lado asin dando el área del número insertado
+    '''
     return lado * lado
 
 
 def perimetro_rectangulo(base: float, altura: float) -> float:
+    '''
+    :base: un valor entero o decimal representando los lados de arriba y abajo del rectangulo
+    :altura: un valor entero o decimal representando los lados izquierdos y derecho del rectangulo
+    :return: devuelve los valores cálculados utilizando la formúla del perimetro
+    '''
     return 2 * (base + altura)
 
 
 def area_circulo(radio: float) -> float:
+    '''
+    :radio: un valor entero o decimal
+    :return: devuelve el resultado del área del circulo, esta llamando a la biblioteca de math para utilizar pi 
+    '''
     return math.pi * radio * radio
 
 
 def area_triangulo(base: float, altura: float) -> float:
+    '''
+    :base: un valor entero o decimal
+    :altura: un valor entero o decimal
+    :return: devuelve el resultado del área del triangulo,
+    '''
     return (base * altura) / 2
 
 
 def hipotenusa(cateto1: float, cateto2: float) -> float:
+    '''
+    :cateto1: un valor entero o decimal
+    :cateto2: un valor entero o decimal
+    :return: devuelve el resultado de la hipotenusa llamando la biblioteca math para usar la raíz cuadrada
+    '''
     return math.sqrt(cateto1 ** 2 + cateto2 ** 2)
 
 
 def distancia_puntos(x1: float, y1: float, x2: float, y2: float) -> float:
+    '''
+    :x1: un valor entero o decimal
+    :y1: un valor entero o decimal
+    :x2: un valor entero o decimal
+    :y2: un valor entero o decimal
+    :return: devuelve la variable distancia
+    '''
     dx = x2 - x1
     dy = y2 - y1
     distancia = math.sqrt(dx ** 2 + dy ** 2)
