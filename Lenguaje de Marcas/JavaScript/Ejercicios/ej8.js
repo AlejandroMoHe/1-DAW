4dfa07f9413cd20bb010b85d044ee4224f775b89
Alejandro <alejandro18032006@gmail.com>
2026-02-09 21:12:06 +0100

    Un pequeño avance del Ejercicio8

---
+++ Lenguaje de Marcas/JavaScript/Ejercicios/ej8.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("formDatos")
+const resultado = document.getElementById("resultado")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const num = document.getElementById("numero").value
+
+})
