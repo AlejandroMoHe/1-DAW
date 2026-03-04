38d0eabcd2973167a9543308d3f09ed0267e7208
Alejandro <alejandro18032006@gmail.com>
2026-03-04 17:31:12 +0100

    Ejercicio 2 y 3 de UD6

---
+++ Programación/UD6 Aspectos Avanzados POO/ejercicio2.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+
+class Empleado:
+    def __init__(self, nombre: str, fecha_inicio: datetime, sueldo: float):
+        self.nombre = nombre
+        self.fecha_inicio = fecha_inicio
+        self.sueldo = sueldo
+
+class EmpleadoFijo(Empleado):
+    def __init__(self, nombre: str, fecha_inicio: datetime, sueldo: float):
+        super().__init__(nombre, fecha_inicio, sueldo)
+    
+    def trineos(self) -> int:
+        hoy = datetime.today()
+        antiguedad = (hoy - self.fecha_inicio).days / 365.25
+        
+
+
+class EmpleadoTemporal(Empleado):
+    def __init__(self, nombre: str, fecha_inicio: datetime, sueldo: float, fecha_fin: datetime):
+        super().__init__(nombre, fecha_inicio, sueldo)
+        self.fecha_fin = fecha_fin
+    
+    def meses_restantes(self) -> int:
+        hoy = datetime.today()
+        tiempo_restante = (hoy - self.fecha_fin)
+        return tiempo_restante
+        
+        
+
+    def ampliar_contrato(self, meses: int) -> None:
+        self.fecha_fin + meses
+
+if __name__ == "__main__":
+    fijo = EmpleadoFijo("Juan", 2022-5-14, 1200)
+    temporal = EmpleadoTemporal("Maria", 2024-8-7, 1000, 2026-3-12)
+
+    #temporal.ampliar_contrato(8)
+    temporal.meses_restantes()
